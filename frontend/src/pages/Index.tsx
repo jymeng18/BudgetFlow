79e54b45a335422c5a8a9c2ec39f30d690173872
jymeng18 <aizeuyn@gmail.com>
2026-01-02 18:34:43 -0800

    shadcn/ui - select and textarea

--- frontend/src/pages/Index.tsx
+++ frontend/src/pages/Index.tsx
@@ -8,3 +8,27 @@
  * Last Modified: Dec 2025
  */
 
+import { useState, useEffect } from "react";
+import { Sidebar } from "@/components/layout/Sidebar";
+import { BudgetHeader } from "@/components/budget/BudgetHeader";
+
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
+
+const Index = () => {
+
+}
+
+export default Index;
